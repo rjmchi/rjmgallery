fdb01572972aff6fcadc319c91f620764f461a1f
rjmchi <robert@rjmprogramming.com>
2020-10-03 11:12:39 -0500

    changes port to use env

--- app.js
+++ app.js
@@ -57,5 +57,5 @@ app.post("/upload", (req, res) => {
     }
   });
 });
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server started on port ${port}...`));
